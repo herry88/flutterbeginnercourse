9dd705cf18b428e75567981b84513ea06b2ca6ce
Herry Prasetyo <herry081288@gmail.com>
2022-06-23 21:16:07 +0700

    pushProject1

---
+++ lib/hello.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HelloWorld extends StatelessWidget {
+  const HelloWorld({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Helloflutter'),
+    );
+  }
+}
